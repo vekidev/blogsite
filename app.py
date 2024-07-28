a7d51a07fc188b1433f7aeeb61a7a43afc624383
vekidev <93841364+vekidev@users.noreply.github.com>
2024-07-28 15:05:19 +0200

    fix i think?

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, request
+import os
+
+app = Flask(__name__)
+blogs = []
+with open("templates/blogs/metadata.txt","r") as f:
+    blogmetaraw = f.read().split('\n')
+    f.close()
+
+blogmeta = {}
+for i in blogmetaraw:
+    line = i.split("`")
+    blogmeta[line[0]] = line[1:]
+
+print(blogmeta)
+
+for ind, blog in enumerate(os.listdir("./templates/blogs")):
+    if blog.endswith(".html"):
+        blogs.append([blog.removesuffix(".html"),"/blog/"+blog.removesuffix(".html"),blogmeta.get(blog,["unknown",blog.removesuffix('.html')])])
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+@app.route(f"/blog/<blogid>")
+def blogpage(blogid):
+    try:
+        return render_template(f"blogs/{blogid}.html",id=blogid)
+    except:
+        return "oh well..."
+    
+@app.route("/blog")
+def blogmain():
+    return render_template("blogpage.html",blogs=blogs)
